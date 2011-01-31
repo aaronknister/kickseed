a4bfa0629d6315867fbef30cb4d5bccca029a30f
Colin Watson <cjwatson@canonical.com>
2011-01-31 17:45:09 +0000

    Preseed partman-lvm/device_remove_lvm when confirming logvol results, since Kickstart doesn't have a separate control with a one-to-one correspondence to this (LP: #708548).

--- handlers/logvol.sh
+++ handlers/logvol.sh
@@ -144,6 +144,7 @@ logvol_handler () {
 
 logvol_final () {
 	if [ "$logvol_confirm" ]; then
+		ks_preseed d-i partman-lvm/device_remove_lvm boolean true
 		ks_preseed d-i partman-lvm/confirm boolean true
 		ks_preseed d-i partman-lvm/confirm_nooverwrite boolean true
 	fi
